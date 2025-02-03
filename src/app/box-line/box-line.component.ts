6f96486d9bed03f6d55a56e74244282f6c4c9f0e
safa <safaelfadhel@gmail.com>
2025-02-03 21:46:53 +0100

    boxline styling

--- src/app/box-line/box-line.component.ts
+++ src/app/box-line/box-line.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { BoxComponent } from '../box/box.component';
 
 @Component({
   selector: 'app-box-line',
   standalone: true,
-  imports: [],
+  imports: [
+    BoxComponent
+  ],
   templateUrl: './box-line.component.html',
   styleUrl: './box-line.component.scss'
 })
 export class BoxLineComponent {
-
+ boxes = [0,1,2,3,4,5,7,8,9]
 }
